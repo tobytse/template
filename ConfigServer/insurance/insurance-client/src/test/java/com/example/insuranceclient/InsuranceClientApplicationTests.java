0522ce9902458128da82f5f96bd21cac4a2dbbb5
Toby Tse <toby.tse@hashkey.com>
2022-07-07 21:13:11 +0800

    add configserver

---
+++ ConfigServer/insurance/insurance-client/src/test/java/com/example/insuranceclient/InsuranceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insuranceclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
